9c62799eaa0094d66b60345bb3f909cbc3f5d0df
Ezra Lazuardy <ezralucio@gmail.com>
2021-12-02 02:16:23 +0700

    Initial commit

--- commander.py
+++ commander.py
@@ -1,11 +1,14 @@
 import os
 import json
 import requests
+import logging
 import paho.mqtt.client as paho
 from paho import mqtt
 from dotenv import load_dotenv
 
 load_dotenv()
+logging.getLogger('requests').setLevel(logging.CRITICAL)
+logging.getLogger('urllib3').setLevel(logging.CRITICAL)
 
 
 class JsonSerializable(object):
@@ -18,88 +21,96 @@ class JsonSerializable(object):
 
 class DeviceType(JsonSerializable):
     def __init__(self, identifier):
-        self.type = "server"
+        self.type = 'server'
         self.id = identifier
 
 
 class FetchStatus(JsonSerializable):
-    def __init__(self, target):
-        self.action = "status"
-        self.target = target
+    def __init__(self, drone):
+        self.action = 'status'
+        self.target_id = drone['target_id']
+        self.target_type = drone['target_type']
 
 
 class UpdateLocation(JsonSerializable):
-    def __init__(self, target, lat, lon):
-        self.action = "location"
-        self.target = target
-        self.lat = lat
-        self.lon = lon
+    def __init__(self, location):
+        self.action = 'location'
+        self.target_id = location['target_id']
+        self.target_type = location['target_type']
+        self.lat = location['lat']
+        self.lon = location['lon']
 
 
 class ResultStatus(JsonSerializable):
-    def __init__(self, current_device, data):
-        self.id = current_device.id
-        self.type = current_device.type
+    def __init__(self, device, data):
+        self.id = device.id
+        self.type = device.type
+        self.target_id = data['id']
+        self.target_type = data['type']
         self.battery = data['battery']
         self.online = int(data['online'])
 
 
 class ResultLocation(JsonSerializable):
-    def __init__(self, current_device, data):
+    def __init__(self, device, data):
         self.result = int(True)
-        self.id = current_device.id
-        self.type = current_device.type
+        self.id = device.id
+        self.type = device.type
+        self.target_id = data['id']
+        self.target_type = data['type']
         self.lat = data['lat']
         self.lon = data['lon']
 
 
-commander_channel = 'adcs/commander'
-client_channel = 'adcs/drones'
 device = DeviceType('commander-server')
+backend_channel = str(os.getenv('HIVEMQ_CLOUD_BROKER_BACKEND_CHANNEL'))
+commander_channel = str(os.getenv('HIVEMQ_CLOUD_BROKER_COMMANDER_CHANNEL'))
+drone_channel = str(os.getenv('HIVEMQ_CLOUD_BROKER_DRONE_CHANNEL'))
 backend_url = 'https://adcs.test/'
 headers = {
+    'Accept': 'application/json',
     'Content-type': 'application/json',
+    'User-Agent': 'mqtt',
     'Key': str(os.getenv('ADCS_BACKED_KEY'))
 }
 qos = int(os.getenv('HIVEMQ_CLOUD_BROKER_QOS', 2))
 
 
 def on_connect(client, userdata, flags, rc, properties=None):
-    print("CONNACK received with code %s" % (rc))
-    client.subscribe(client_channel, qos=qos)
+    print('CONNACK received with code %s' % (rc))
+    client.subscribe(drone_channel, qos=qos)
+    client.subscribe(backend_channel, qos=qos)
 
 
 def on_publish(client, userdata, mid):
-    print("published: %s" % (str(mid)))
+    print('published: %s' % (str(mid)))
 
 
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print("subscribed: %s" % (str(mid)))
+    print('subscribed: %s' % (str(mid)))
 
 
 def on_message(client, userdata, msg):
-    print("received: %s %s %s" % (msg.topic, str(msg.qos), str(msg.payload)))
+    print('received: %s %s %s' % (msg.topic, str(msg.qos), str(msg.payload)))
     try:
-        payload = json.loads(str(msg.payload.decode("utf-8")))
+        payload = json.loads(str(msg.payload.decode('utf-8')))
         action = payload['action']
-        if payload['id'] is not None:
-            target = payload['id']
-            if action == "status":
-                publish_status(target, payload)
-            elif action == "location":
-                publish_location(target, payload)
+        if action == 'status':
+            if msg.topic == drone_channel:
+                publish_status(payload)
+            elif msg.topic == backend_channel:
+                fetch_status(payload)
             else:
-                print("error: unknown payload action, skipping...")
-        elif payload['target'] is not None:
-            target = payload['target']
-            if action == "status":
-                fetch_status(target)
-            elif action == "location":
-                update_location(target, payload)
+                print('error: unknown topic, skipping...')
+        elif action == 'location':
+            if msg.topic == drone_channel:
+                publish_location(payload)
+            elif msg.topic == backend_channel:
+                update_location(payload)
             else:
-                print("error: unknown payload action, skipping...")
+                print('error: unknown topic, skipping...')
         else:
-            print("error: unknown payload format, skipping...")
+            print('error: unknown action, skipping...')
     except Exception as e:
         print(str(e))
 
@@ -108,27 +119,36 @@ def fetch_status(target):
     publish_action(FetchStatus(target))
 
 
-def update_location(target, location):
-    publish_action(UpdateLocation(target, location['lat'], location['lon']))
+def update_location(location):
+    publish_action(UpdateLocation(location))
 
 
 def publish_action(action: JsonSerializable):
     client.publish(commander_channel, payload=action.to_json(), qos=qos)
 
 
-def publish_status(target, status):
-    http_post("api/drones", ResultStatus(device, status).to_json())
+def publish_status(status):
+    http_post('api/drones', ResultStatus(device, status).to_json())
 
 
-def publish_location(target, location):
-    http_post("api/drones", ResultLocation(device, location).to_json())
+def publish_location(location):
+    http_post('api/drones', ResultLocation(device, location).to_json())
 
 
 def http_post(path, data):
     print('posting result to ADCS Backend...')
-    response = requests.post(backend_url + path, data=json.loads(data), headers=headers, timeout=5)
+    response = requests.post(
+        headers=headers,
+        url=backend_url + path,
+        data=str(data),
+        timeout=5,
+        verify=bool(int(os.getenv('SSL_VERIFICATION')))
+    )
     print('status code: %d' % (int(response.status_code)))
-    print(response.text)
+    try:
+        print(json.loads(str(response.text)))
+    except:
+        print('error: backend response is not a valid JSON format, skipping...')
 
 
 client = paho.Client(client_id=device.id, userdata=device.to_json(), protocol=paho.MQTTv5)
--- drone.py
+++ drone.py
@@ -2,16 +2,22 @@ import os
 import sys
 import json
 import argparse
+import time
+
 import paho.mqtt.client as paho
 from paho import mqtt
 from dotenv import load_dotenv
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--id", help="Drone identifier")
+parser.add_argument('-i', '--id', help='Drone identifier')
+parser.add_argument('-t', '--type', help='Drone type')
 args = parser.parse_args()
 
 if not args.id:
-    sys.exit("Drone identifier must be set! (using --id)")
+    sys.exit('Drone identifier must be set! (using --id)')
+
+if not args.type:
+    sys.exit('Drone type must be set! (using --type)')
 
 load_dotenv()
 
@@ -25,9 +31,9 @@ class JsonSerializable(object):
 
 
 class DeviceType(JsonSerializable):
-    def __init__(self, identifier):
-        self.type = "drone"
+    def __init__(self, identifier, type):
         self.id = identifier
+        self.type = type
 
 
 class ResultStatus(JsonSerializable):
@@ -48,40 +54,40 @@ class ResultLocation(JsonSerializable):
         self.lon = lon
 
 
-commander_channel = "adcs/commander"
-client_channel = "adcs/drones"
-device = DeviceType(args.id)
+device = DeviceType(args.id, args.type)
+commander_channel = str(os.getenv('HIVEMQ_CLOUD_BROKER_COMMANDER_CHANNEL'))
+drone_channel = str(os.getenv('HIVEMQ_CLOUD_BROKER_DRONE_CHANNEL'))
 qos = int(os.getenv('HIVEMQ_CLOUD_BROKER_QOS', 2))
 
 
 def on_connect(client, userdata, flags, rc, properties=None):
-    print("CONNACK received with code %s" % (rc))
+    print('CONNACK received with code %s' % (rc))
     client.subscribe(commander_channel, qos=qos)
-    publish_status()
 
 
 def on_publish(client, userdata, mid):
-    print("published: %s" % (str(mid)))
+    print('published: %s' % (str(mid)))
 
 
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print("subscribed: %s" % (str(mid)))
+    print('subscribed: %s' % (str(mid)))
 
 
 def on_message(client, userdata, msg):
-    print("received: %s %s %s" % (msg.topic, str(msg.qos), str(msg.payload)))
+    print('received: %s %s %s' % (msg.topic, str(msg.qos), str(msg.payload)))
     try:
-        payload = json.loads(str(msg.payload.decode("utf-8")))
-        target = payload['target'].strip()
+        payload = json.loads(str(msg.payload.decode('utf-8')))
+        target = payload['target_id'].strip()
+        type = payload['target_type'].strip()
         action = payload['action'].strip()
-        if target != device.id:
+        if target != device.id and type != device.type:
             return
-        if action == "status":
+        if action == 'status':
             publish_status()
-        elif action == "location":
+        elif action == 'location':
             publish_location(payload)
         else:
-            print("error: unknown payload action, skipping...")
+            print('error: unknown action, skipping...')
     except Exception as e:
         print(str(e))
 
@@ -95,7 +101,7 @@ def publish_location(location):
 
 
 def publish_result(result):
-    client.publish(client_channel, payload=result.to_json(), qos=qos)
+    client.publish(drone_channel, payload=result.to_json(), qos=qos)
 
 
 client = paho.Client(client_id=args.id, userdata=device.to_json(), protocol=paho.MQTTv5)
@@ -106,4 +112,7 @@ client.on_publish = on_publish
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 client.username_pw_set(str(os.getenv('HIVEMQ_CLOUD_BROKER_USERNAME')), str(os.getenv('HIVEMQ_CLOUD_BROKER_PASSWORD')))
 client.connect(str(os.getenv('HIVEMQ_CLOUD_BROKER_HOST')), int(os.getenv('HIVEMQ_CLOUD_BROKER_PORT')))
-client.loop_forever()
+
+while client.loop() == 0:
+    publish_status()
+    time.sleep(4)
